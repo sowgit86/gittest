dc96abb0f14f28116d10fdced5136d3edd445dfb
sowmi <sowmi@my-ububtu>
2018-08-28 09:42:30 -0500

    Factorial first commit

---
+++ src/gitfact.java
@@ -0,0 +1,28 @@
+
+public class gitfact {
+
+
+	 
+	int n1=5,fact=1; 
+	
+	public int findfactorial() {
+	
+		for(int i = 1; i<=5; i++) {
+			
+			fact = fact *i;
+		}
+	//System.out.println("the factorial of 5 id : " + fact);
+		
+		return fact;
+		
+	}
+	public static void main(String[] args) {
+		
+		gitfact a = new gitfact();
+		System.out.println("Hello Java");
+		System.out.println("the factorial of 5 id : " + a. findfactorial());
+
+	}
+
+}
+	
